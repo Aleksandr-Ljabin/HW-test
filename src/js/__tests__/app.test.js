ca66acbd9865d47e8d347d934d5548085f01c0ef
Aleksandr-Ljabin <frontdevell@gmail.com>
2022-02-06 16:14:04 +0300

    Исправление ДЗ

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,8 +1,22 @@
 import hero from '../app';
 
 test('getting information about the characters health', () => {
+  const input = { name: 'Маг', health: 10 };
+  const expected = 'critical';
+  const received = hero(input);
+  expect(received).toEqual(expected);
+});
+
+test('getting information about the characters health', () => {
   const input = { name: 'Маг', health: 32 };
   const expected = 'wounded';
+  const received = hero(input);
+  expect(received).toEqual(expected);
+});
+
+test('getting information about the characters health', () => {
+  const input = { name: 'Маг', health: 70 };
+  const expected = 'healthy';
   const received = hero(input);
   expect(received).toEqual(expected);
 });
